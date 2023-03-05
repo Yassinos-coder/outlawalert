7ce756b9e14a8b456ff02a6439319199eb4c7b76
Yassinos-Coder <castroyassine0@gmail.com>
2023-03-05 16:48:18 +0100

    Small Logic Changes

--- backend/core.js
+++ backend/core.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.info('Database connection granted');
     db_connection_success = true
 }).catch(error => {
+    db_connection_success = false
     console.error('Database connection error:', error.message);
 });
 
