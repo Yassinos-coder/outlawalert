d81702014e4cc6172098aff61688032a726715c6
Yassinos-Coder <castroyassine0@gmail.com>
2023-03-05 17:22:14 +0100

    Added 404ERROR HANDLER

--- frontend/src/Pages/NotFound/NotFound.jsx
+++ frontend/src/Pages/NotFound/NotFound.jsx
@@ -1,11 +1,16 @@
 import './NotFound.css'
+import PicError from '../../assets/img/404Error.png'
+import {Link} from 'react-router-dom'
 
 
 const NotFound = () => {
   return (
     <>
       <div className="notfound">
-        404 ERROR NOT FOUND
+        <img className='errorPic' src={PicError} alt="" />
+        <p className="text">
+          It looks like you're trying to access an unlocated route <br /> Please go back to <Link className='a-homepage-error' to="/">HomePage</Link>
+        </p>
       </div>
     </>
   )
