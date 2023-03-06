8b02bcbd6fd1f4dceaa38201c6907bf3d213ac56
Yassinos-Coder <castroyassine0@gmail.com>
2023-03-06 12:10:09 +0100

    Added OffCanvas, Fixed Styling Issues

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,59 @@
+import { faBars, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import MenuOffCanvas from '../MenuOffCanvas/MenuOffCanvas'
+import './Header.css'
+
+const Header = () => {
+  const navigate = useNavigate()
+  const [isMenuVisible, setIsMenuVisible] = useState(false)
+
+  const handleLogOut = () => {
+    navigate("/");
+    localStorage.removeItem("tokenKey");
+  };
+
+  const handleMenuToggle = () => {
+    setIsMenuVisible(!isMenuVisible)
+  }
+
+  return (
+    <div className="header-homedash">
+      <MenuOffCanvas isMenuToggle={isMenuVisible} />
+        <div className="headerMenu">
+          <div className="item1">
+            <FontAwesomeIcon
+              style={{
+                fontSize: "2.5rem",
+                paddingTop: "10px",
+                paddingLeft: "15px",
+              }}
+              className='faBarsHomeDash'
+              icon={faBars}
+              onClick={handleMenuToggle}
+            />
+          </div>
+          <div className="item2">
+            <h3 className="h3-homedash-item2">
+              Bienvenue, {localStorage.firstname}{" "}
+            </h3>
+          </div>
+          <div className="item3">
+            <FontAwesomeIcon
+              className="faLogOut"
+              onClick={handleLogOut}
+              style={{
+                fontSize: "2.5rem",
+                paddingTop: "10px",
+                paddingLeft: "15px",
+              }}
+              icon={faRightFromBracket}
+            />
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Header
