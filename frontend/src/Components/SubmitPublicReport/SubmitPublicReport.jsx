f162ae76b77f10dc941dc421cc2b21bdd276ec00
Yassinos-Coder <castroyassine0@gmail.com>
2023-03-11 15:37:31 +0100

    Added AddReport, Resumed ReportModelDB/Class, Switched to Nested Routes

---
+++ frontend/src/Components/SubmitPublicReport/SubmitPublicReport.jsx
@@ -0,0 +1,14 @@
+import './SubmitPublicReport.css'
+import React from 'react'
+
+const SubmitPublicReport = () => {
+  return (
+    <>
+      <div className="submitPublicReport">
+        
+      </div>
+    </>
+  )
+}
+
+export default SubmitPublicReport
