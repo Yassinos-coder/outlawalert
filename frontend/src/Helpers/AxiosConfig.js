7b8033fcf8dfcf799ebf89690fa1afafa6b0f23d
Yassinos-Coder <castroyassine0@gmail.com>
2023-03-16 17:29:32 +0100

    Updated Loots of stuff

--- frontend/src/Helpers/AxiosConfig.js
+++ frontend/src/Helpers/AxiosConfig.js
@@ -5,8 +5,7 @@ const tokenKey = localStorage.getItem('tokenKey') ? localStorage.tokenKey : ''
 const AxiosConfig = axios.create({
     baseURL: 'http://192.168.4.4:8009/',
     headers: {
-        authorization: `bearer ${tokenKey}`,
-        Coder: 'Yassinos-Coder'
+        authorization: `bearer ${tokenKey}`
     }
 })
 
